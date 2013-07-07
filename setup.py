34df92ce56bba33462d8b47f8b43a14bd3926564
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-07-07 15:58:20 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+import sys
+
+
+setup(
+    name="ndtypes",
+    description="Types for array-oriented domain-specific languages embedded in Python.", 
+    long_description="",
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Topic :: Software Development :: Libraries',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 2.7',
+                 ],
+    author="Alex Rubinsteyn",
+    author_email="alexr@cs.nyu.edu",
+    license="BSD",
+    version="0.1",
+    url="http://github.com/iskandr/ndtypes",
+    packages=[ 'ndtypes'],
+    package_dir={ '' : '.' },
+    requires=[ 'treelike'] 
+    )
