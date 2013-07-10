d887b13a53c83c76b78b41742228d9772821e84d
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-07-10 14:06:36 -0400

    added module init

--- ndtypes/__init__.py
+++ ndtypes/__init__.py
@@ -1,1 +1,23 @@
+from array_type import ArrayT, make_array_type
+from array_type import elt_type, elt_types, rank
+from array_type import get_rank, lower_rank
+from array_type import lower_rank, increase_rank, increase_ranks
 
+from slice_type import SliceT, make_slice_type
+
+from tuple_type import TupleT, make_tuple_type 
+
+from scalar_types import ScalarT
+from scalar_types import IntT, FloatT, BoolT, SignedT, UnsignedT, ComplexT
+from scalar_types import Int8, Int16, Int32, Int64
+from scalar_types import UInt8, UInt16, UInt32, UInt64
+from scalar_types import Bool, Float32, Float64
+from scalar_types import Complex64, Complex128
+from scalar_types import ConstIntT
+from scalar_types import is_scalar_subtype, is_scalar, all_scalars, 
+from scalar_types import from_dtype, from_char_code 
+
+from ptr_type import PtrT, ptr_type 
+
+import dtypes
+import type_conv   
